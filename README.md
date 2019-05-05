a410f8a0ba6316ff6783f136199676910385f677
Shelley Arnold <shelle.kay@gmail.com>
2019-05-05 18:20:27 -0500

    edit button

--- README.md
+++ README.md
@@ -1,18 +1,13 @@
-#Daily Journal
+# Daily Journal
 
-
-Linking css stylesheets. 
-
-
-Styling based on css knowledge
-
-
-Adding forms and fieldsets.
-
-
-
-
-Linking js file.
-
-
-Adding objects and appending to the DOM.
+Each brach has been developed and updated with the following topics:
+1. Linking css stylesheets. 
+1. Styling based on css knowledge
+1. Adding forms and fieldsets.
+1. Linking js file.
+1. Adding objects and appending to the DOM.
+1. Modularizing code
+1. Adding event listeners, basic validation  and posting to the database
+1. Adding radio buttons and sorting through entries and using filter()
+1. Adding delete button and removing info from the database
+1. Adding an edit button
---
+++ src/scripts/dbFunction.js
@@ -0,0 +1,24 @@
+//posts new entry to database
+submitNewEntry.addEventListener("click", () => {
+    let newEntry = {
+        date: grabNewDate.value,
+        concept: grabNewConcept.value,
+        entry: grabNewEntry.value,
+        mood: grabNewMood.value
+    }
+    //validation for info to be complete
+    if (newEntry.date !== "" && newEntry.concept !== "" && newEntry.entry !== "" && newEntry.mood !== "") {
+        if(buttonMode === "create")
+            makeEntry(newEntry)
+        //update's entry
+        else
+            updateEntry(updateEntryId, newEntry);
+    }else {
+        let myAlertMessage = "please adding missing content: "
+        if(newEntry.date == ""){myAlertMessage += `add date `}
+        if(newEntry.concept == ""){myAlertMessage += `add concepts covered `}
+        if(newEntry.entry == ""){myAlertMessage += `add journal entry `}
+        if(newEntry.mood == ""){myAlertMessage += `add mood`}
+        alert(myAlertMessage)
+    }
+});
